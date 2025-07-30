f320b251f029730a18ed212dda057ff400563f50
truong0987654321 <trongtruong0908@gamil.com>
2025-07-30 14:37:54 +0700

    Remove src, public folder from Git

--- src/components/ui/menu/action_menu.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export const ActionMenu = () => {
-    return (
-        <div>A</div>
-    )
-}
--- src/store/index.js
+++
@@ -1,7 +0,0 @@
-
-import { create } from "zustand";
-import { createAuthSlice } from "./slices/auth-slice";
-
-export const useAppstore = create()((...a)=>({
-    ...createAuthSlice(...a),
-}));
